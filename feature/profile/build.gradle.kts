5afac5a8a6c89b6f92b073bd21cf0cbe2fd89363
Игорь Ергин <gora.ergin@gmail.com>
2026-04-14 14:53:48 +0500

    Feature: Добавлена зависимость core.network в модуль profile

--- feature/profile/build.gradle.kts
+++ feature/profile/build.gradle.kts
@@ -10,6 +10,7 @@ dependencies {
     implementation(projects.core.ui)
     implementation(projects.core.navigation)
     implementation(projects.data.domain)
+    implementation(projects.core.network)
     implementation(libs.androidx.compose.material.icons)
     implementation(libs.androidx.compose.material.icons.extended)
 }
